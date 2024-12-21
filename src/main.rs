29eb95059848b9b2b824b8f3bf1b7fb8b8a6c703
Jochen Stierberger <blitz@mbp14.local>
2024-12-21 16:28:12 -0600

    feat: Add sqlx and postgres config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use email_newsletter::configuration::get_configuration;
+use email_newsletter::startup::run;
 use std::net::TcpListener;
-
-use email_newsletter::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
 
+use email_newsletter::startup::run;
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
-    let server = email_newsletter::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
