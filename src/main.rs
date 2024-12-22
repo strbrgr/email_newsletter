e5f94ebc980cff3b1a5dc1e38d725dfb3b93e734
Jochen Stierberger <blitz@mbp14.local>
2024-12-22 14:52:16 -0600

    feat: Add PgPool implementation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use email_newsletter::configuration::get_configuration;
 use email_newsletter::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    // We have removed the hard-coded `8000` - it's now coming from our settings!
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failted to connect to Pg.");
+    let address = format!("127.0.0.1:{}", &configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+
+    run(listener, connection_pool)?.await
 }
